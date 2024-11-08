93f623fd03c6689ed847a29fdc9b9e77d712647c
Matthew <99048876+JustFiesta@users.noreply.github.com>
2024-11-08 16:17:11 +0100

    Creating folders with default mode

--- project1-systemcalls/mkdirmb.c
+++ project1-systemcalls/mkdirmb.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <sys/stat.h>
 #include <linux/limits.h>
-#include <errno.h>
-#include <string.h>
 
+// Get mkdir syscall number from unistd.h header
 #define MKDIR_SYSCALL_NUM  __NR_mkdir
-#define CHMOD_SYSCALL_NUM  __NR_chmod
 
+// Help function
 void print_usage(const char *progname) {
-    fprintf(stderr, "Usage: %s [-p] [-m mode] directory_path\n", progname);
+    fprintf(stderr, "Usage: %s [-p] directory_path\n", progname);
 }
 
-// Funkcja do wywoływania syscalla mkdir
+// Mkdir syscall function
 int syscall_mkdir(const char *path, mode_t mode) {
     return syscall(MKDIR_SYSCALL_NUM, path, mode);
 }
 
-// Funkcja do rekurencyjnego tworzenia katalogów (gdy -p jest użyte)
+// Recursievelly create parent 
 int create_parents(const char *path, mode_t mode) {
     char dir_path[PATH_MAX];
     strncpy(dir_path, path, PATH_MAX);
@@ -27,7 +28,7 @@ int create_parents(const char *path, mode_t mode) {
     for (char *p = dir_path + 1; *p; p++) {
         if (*p == '/') {
             *p = '\0';
-            if (syscall_mkdir(dir_path, mode) != 0 && errno != EEXIST) {
+            if (syscall_mkdir(dir_path, 0777) != 0 && errno != EEXIST) {
                 perror("Failed to create parent directory");
                 return -1;
             }
@@ -44,49 +45,48 @@ int create_parents(const char *path, mode_t mode) {
 int main(int argc, char *argv[]) {
     int opt;
     int create_parents_flag = 0;
-    mode_t mode = 0755; // Domyślne prawa dostępu, jeśli nie podano innych
 
-    // Przetwarzanie argumentów z użyciem getopt
-    while ((opt = getopt(argc, argv, "pm:")) != -1) {
+    // Default access mode
+    mode_t mode = 0755;
+
+    // getopt user interface for getting arguments
+    while ((opt = getopt(argc, argv, "ph")) != -1) {
         switch (opt) {
             case 'p':
-                create_parents_flag = 1; // Ustawienie flagi `-p`
+                create_parents_flag = 1; // set flag for -p
                 break;
-            case 'm':
-                mode = strtol(optarg, NULL, 8); // Konwersja uprawnień do systemu ósemkowego
-                if (mode == 0 && errno == EINVAL) {
-                    fprintf(stderr, "Invalid mode: %s\n", optarg);
-                    return EXIT_FAILURE;
-                }
-                break;
+            case 'h':
+                print_usage(argv[0]);
+                return EXIT_SUCCESS;
             default:
                 print_usage(argv[0]);
                 return EXIT_FAILURE;
         }
     }
 
-    // Sprawdza, czy podano ścieżkę katalogu
+    // Check if path was provided
     if (optind >= argc) {
-        fprintf(stderr, "Expected directory path\n");
+        fprintf(stderr, "Error: No directory path provided.\n");
         print_usage(argv[0]);
         return EXIT_FAILURE;
     }
 
-    char *dir_path = argv[optind];
+    // Get path to variable
+    const char *dir_path = argv[optind];
 
-    // Tworzenie katalogu z flagą -p lub bez
+    // Try to create folder/folders
+    int result;
     if (create_parents_flag) {
-        if (create_parents(dir_path, mode) != 0) {
-            fprintf(stderr, "Failed to create directories for path: %s\n", dir_path);
-            return EXIT_FAILURE;
-        }
+        result = create_parents(dir_path, mode);
     } else {
-        if (syscall_mkdir(dir_path, mode) != 0) {
-            perror("Failed to create directory");
-            return EXIT_FAILURE;
-        }
+        result = syscall_mkdir(dir_path, mode);
     }
 
-    printf("Directory created successfully: %s\n", dir_path);
+    if (result != 0) {
+        fprintf(stderr, "Error creating directory: %s\n", strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    printf("Directory created successfully.\n");
     return EXIT_SUCCESS;
 }
